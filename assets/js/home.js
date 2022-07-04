d70f6109227c73484acd1a6109a9e3046fe30116
Sheikh Areeb <67228908+Fighter15c@users.noreply.github.com>
2022-07-05 04:50:50 +0500

    ok

--- assets/js/home.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $('homebtn').click(function(){
-        $.get("sample.txt", function(data, status){
-            $('.card-text').innerHTML(data);    
-        })
-    })  
-})
--- server/model/ordermodel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-var schemaOrder = new mongoose.Schema({
-  customerEmail: String,
-  cakes: String,
-});
-
-const OrderDB = mongoose.model("Order", schemaOrder);
-module.exports = OrderDB;
